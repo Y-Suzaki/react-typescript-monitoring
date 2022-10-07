fc7acc2a74492fe5913c0669e31f3c27bd03e49b
y-suzaki <geranium04.24.1981@gmail.com>
2022-10-07 15:20:23 +0900

    初期設定ファイルの追加

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', '@typescript-eslint'],
+  root: true,
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
