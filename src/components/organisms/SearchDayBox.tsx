430c67da86631d0acec36fefe67ebc2cea699b36
y-suzaki <geranium04.24.1981@gmail.com>
2022-10-20 07:07:07 +0900

    Mod search box layout.

--- src/components/organisms/SearchDayBox.tsx
+++ src/components/organisms/SearchDayBox.tsx
@@ -1,4 +1,15 @@
-import { HStack, Input } from '@chakra-ui/react';
+import {
+  Box,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react';
 import { ChangeEvent, FC, memo, useState } from 'react';
 import { PrimaryButton } from '../atoms/button/PrimaryButton';
 
@@ -22,12 +33,28 @@ export const SearchDayBox: FC<Props> = memo(function SearchDayBox(props) {
   };
 
   return (
-    <HStack p={2} borderRadius={'md'}>
-      <Input width={'25%'} placeholder="IMEI" value={imei} onChange={onChangeImei} />
-      <Input width={'25%'} placeholder="2022-10-01" value={day} onChange={onChangeDay} />
-      <PrimaryButton loading={isLoading} onClick={() => onClickSearch(imei, day)}>
-        Search
-      </PrimaryButton>
+    <HStack p={2} borderRadius={'md'} alignItems="flex-end">
+      <FormControl width={'25%'}>
+        <FormLabel fontSize="7px" color="red.400">
+          IMEI (required)
+        </FormLabel>
+        <Input placeholder="Enter IMEI" required value={imei} onChange={onChangeImei} />
+      </FormControl>
+      <FormControl width={'25%'}>
+        <FormLabel fontSize="7px">Day</FormLabel>
+        <Input
+          placeholder="Enter day. yyyy-MM-dd"
+          _placeholder={{ color: 'gray.300' }}
+          required
+          value={day}
+          onChange={onChangeImei}
+        />
+      </FormControl>
+      <Box justifyContent="bottom">
+        <PrimaryButton loading={isLoading} onClick={() => onClickSearch(imei, day)}>
+          Search
+        </PrimaryButton>
+      </Box>
     </HStack>
   );
 });
